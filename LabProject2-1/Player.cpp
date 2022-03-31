e5748597c37c65d4c66605f6ce106e683889223d
FrouteLabtop <froute99619@gmail.com>
2022-03-31 12:17:42 +0900

    Done with Player class Need to do Scene class

--- LabProject2-1/Player.cpp
+++ LabProject2-1/Player.cpp
@@ -1,2 +1,33 @@
+
 #include "stdafx.h"
 #include "Player.h"
+
+namespace _3DLab {
+	void Player::SetPosition(float x, float y, float z) {
+		GameObject::SetPosition(x, y, z);
+		if (m_pCamera) {
+			m_pCamera->SetPosition(x, y, z);
+		}
+	}
+
+	void Player::SetRotation(float x, float y, float z) {
+		GameObject::SetRotation(x, y, z);
+		if (m_pCamera) {
+			m_pCamera->SetRotation(x, y, z);
+		}
+	}
+
+	void Player::Move(float x, float y, float z) {
+		if (m_pCamera) {
+			m_pCamera->Move(x, y, z);
+		}
+		GameObject::Move(x, y, z);
+	}
+
+	void Player::Rotate(float pitch, float yaw, float roll) {
+		if (m_pCamera) {
+			m_pCamera->Rotate(pitch, yaw, roll);
+		}
+		GameObject::Rotate(pitch, yaw, roll);
+	}
+}
--- LabProject2-1/Player.h
+++ LabProject2-1/Player.h
@@ -1,5 +1,32 @@
-#pragma once
-class Player
-{
-};
 
+
+#pragma once
+#include "GameObject.h"
+#include "Camera.h"
+
+namespace _3DLab {
+	class Player : public GameObject {
+	public:
+		Player() {}
+		virtual ~Player() {
+			if (m_pCamera) {
+				delete m_pCamera;
+			}
+		}
+
+		void SetPosition(float x, float y, float z);
+		void SetRotation(float x, float y, float z);
+
+		void Move(float x, float y, float z);
+		void Rotate(float x, float y, float z);
+
+		void SetCamera(Camera* pCamera) {
+			m_pCamera = pCamera;
+		}
+		Camera* GetCamera() {
+			return m_pCamera;
+		}
+	private:
+		Camera* m_pCamera = nullptr;
+	};
+}
