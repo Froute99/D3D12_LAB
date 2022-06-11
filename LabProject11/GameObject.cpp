d510a7f81f327c9532c5329437fee69ccc163911
Froute99 <froute99619@gmail.com>
2022-06-11 21:17:05 +0900

    따라하기12
    player.h 까지 했고
    Player.cpp 할 차례

---
+++ LabProject11/GameObject.cpp
@@ -0,0 +1,125 @@
+#include "stdafx.h"
+#include "Shader.h"
+#include "GameObject.h"
+
+CGameObject::CGameObject() {
+	XMStoreFloat4x4(&m_xmf4x4World, XMMatrixIdentity());
+}
+
+CGameObject::~CGameObject() {
+	if (m_pMesh)
+		m_pMesh->Release();
+	if (m_pShader) {
+		m_pShader->ReleaseShaderVariables();
+		m_pShader->Release();
+	}
+}
+
+void CGameObject::CreateShaderVariables(ID3D12Device* pd3dDevice,
+	ID3D12GraphicsCommandList* pd3dCommandList)
+{
+}
+
+void CGameObject::ReleaseShaderVariables()
+{
+}
+
+void CGameObject::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList)
+{
+	XMFLOAT4X4 xmf4x4World;
+	XMStoreFloat4x4(&xmf4x4World, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4World)));
+	//객체의 월드 변환 행렬을 루트 상수(32-비트 값)를 통하여 셰이더 변수(상수 버퍼)로 복사한다.
+	pd3dCommandList->SetGraphicsRoot32BitConstants(0, 16, &xmf4x4World, 0);
+}
+
+void CGameObject::MoveStrafe(float fDistance)
+{
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Right = GetRight();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Right, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::MoveUp(float fDistance)
+{
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Up = GetUp();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Up, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::MoveForward(float fDistance)
+{
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Look = GetLook();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Look, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::Rotate(XMFLOAT3* pxmf3Axis, float fAngle) {
+	XMMATRIX mtxRotate = XMMatrixRotationAxis(XMLoadFloat3(pxmf3Axis),
+		XMConvertToRadians(fAngle));
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::Rotate(float fPitch, float fYaw, float fRoll)
+{
+	XMMATRIX mtxRotate = XMMatrixRotationRollPitchYaw(XMConvertToRadians(fPitch),
+		XMConvertToRadians(fYaw), XMConvertToRadians(fRoll));
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::SetShader(CShader* pShader) {
+	if (m_pShader)
+		m_pShader->Release();
+	m_pShader = pShader;
+	if (m_pShader)
+		m_pShader->AddRef();
+}
+
+void CGameObject::SetMesh(CMesh* pMesh) {
+	if (m_pMesh)
+		m_pMesh->Release();
+	m_pMesh = pMesh;
+	if (m_pMesh)
+		m_pMesh->AddRef();
+}
+
+void CGameObject::ReleaseUploadBuffers() {
+	// 정점 버퍼를 위한 업로드 버퍼를 소멸시킨다.
+	if (m_pMesh)
+		m_pMesh->ReleaseUploadBuffers();
+}
+
+void CGameObject::Animate(float fTimeElapsed) {
+	 
+}
+
+void CGameObject::OnPrepareRender() {
+
+}
+
+void CGameObject::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera)
+{
+	OnPrepareRender();
+
+	UpdateShaderVariables(pd3dCommandList);
+
+	if (m_pShader) m_pShader->Render(pd3dCommandList, pCamera);
+
+	if (m_pMesh) m_pMesh->Render(pd3dCommandList);
+}
+
+CRotatingObject::CRotatingObject() {
+	m_xmfRotationAxis = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	m_fRotationSpeed = 90.0f;
+}
+
+CRotatingObject::~CRotatingObject() {
+
+}
+
+void CRotatingObject::Animate(float fTimeElapsed) {
+	CGameObject::Rotate(&m_xmfRotationAxis, m_fRotationSpeed * fTimeElapsed);
+}
+
